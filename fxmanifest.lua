707c4d7cedbea9058e3d6d87f30912a9519cf271
SPOOKZ <53491846+spcckz@users.noreply.github.com>
2024-11-25 16:07:52 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean' -- Compatible with the latest FiveM version
+game 'gta5'
+
+author 'YourName'
+description 'Custom Dependency-Free Menu Script'
+version '1.0.0'
+
+client_scripts {
+    'client.lua', -- Ensure this matches the name of your client script
+    'paint.lua' -- The new paint cycling script
+}
